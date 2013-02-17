725938b343769ddf9670b797f3d788abecb8c3c5
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2013-02-16 20:54:38 -0500

    Fix for coverity

--- jlibtool.c
+++ jlibtool.c
@@ -2346,7 +2346,7 @@ static void cleanup_tmp_dir(const char *dirname)
             strcpy(fullname, dirname);
             strcat(fullname, "/");
             strcat(fullname, entry->d_name);
-            remove(fullname);
+            (void) remove(fullname);
         }
     }
 
